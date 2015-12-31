1bfe0673e3103d938704f7681ee0f370e08147b9
GrooveMiami <groovemiami@yahoo.com>
2015-12-30 19:59:44 -0500

    minor

--- js/mailchimp.js
+++ js/mailchimp.js
@@ -1,5 +1,5 @@
 $('#mc-form').ajaxChimp({
-    url: '//github.us12.list-manage.com/subscribe/post?u=e84cb3b9b9a5553fa38c3fa40&amp;id=f9b3bb1e9b'
+    url: '//github.us12.list-manage.com/subscribe/post?u=e84cb3b9b9a5553fa38c3fa40&amp;id=f9b3bb1e9b',
     callback: callbackFunction
 });
 
